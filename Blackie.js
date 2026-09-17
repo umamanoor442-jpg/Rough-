b7c9541dc940359319c4aca3328947382f2a67ae
Umama Noor <umamanoor442@gmail.com>
2026-09-17 16:10:08 +0500

    Add files via upload

---
+++ Blackie.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+function calculate(){
+var num1= +prompt("Enter first number")
+var  op= prompt("Enter Operation")
+var  num2= +prompt("Enter Second number")
+
+
+
+if(op== "+" | op== "-" | op== "*" | op== "/" | op== "%" ){
+if(op== "+"){
+    var add = num1 + num2 
+    alert(add)
+
+}
+else if(op== "-"){
+    var sub = num1 - num2 
+    alert(sub)
+}
+else if(op=="*"){
+    var multi = num1 * num2 
+    alert(multi)
+}
+else if(op=="/"){
+    var divide = num1 / num2 
+    alert(divide)
+}
+else if(op=="%"){
+var mod = num1 % num2 
+alert(mod)
+}
+}
+else{
+    alert("that is not an assignment")
+}
+}
+
+
+
+
+
+
